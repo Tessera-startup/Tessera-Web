7c7c07c5bf93fd4be510ec493a061d60d2f5e8ab
temycodes <temycodes@gmail.com>
2023-09-20 18:25:08 +0100

    build: login section

---
+++ src/pages/login.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+const SponsorLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Layout>
+      <div className="container mx-auto mt-8">
+        {loggedIn ? (
+          <div>
+            <h2 className="text-3xl font-semibold mb-4">Welcome, Sponsor!</h2>
+          </div>
+        ) : (
+          <div className="max-w-screen-sm mx-auto mt-9 px-5">
+            <h2 className="text-3xl font-semibold mb-4">Sponsor Login</h2>
+            <form onSubmit={handleLogin}>
+              <div className="mb-4">
+                <label htmlFor="email" className="block text-gray-600">
+                  Email:
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="border border-gray-300 rounded px-3 py-2 w-full"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="block text-gray-600">
+                  Password:
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="border border-gray-300 rounded px-3 py-2 w-full"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-gray-700 text-white px-6 py-2 rounded-md hover:bg-gray-500 transition duration-300 transform hover:scale-105"
+              >
+                Login
+              </button>
+            </form>
+            <p className="mt-4">
+              Don't have an account?{" "}
+              <Link href="/signup" className="text-blue-600">
+                Sign up here
+              </Link>
+            </p>
+          </div>
+        )}
+      </div>
+    </Layout>
+  );
+};
+
+export default SponsorLogin;
--- src/pages/ticket.js
+++ src/pages/ticket.js
@@ -39,7 +39,7 @@ const nftTickets = [
 const NftTicket = () => {
   return (
     <Layout>
-      <div className="container mx-auto">
+      <div className="container mx-auto mt-5">
         <h1 className="text-3xl font-bold mb-4">NFT Tickets</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {nftTickets.map((nftTicket) => (
