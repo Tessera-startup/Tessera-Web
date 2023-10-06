1e316a02e242518a0aed7eeb6b04bbecf9bd9ad3
temycodes <temycodes@gmail.com>
2023-10-06 15:04:45 +0100

    build: merged

--- src/pages/tickets.js
+++ src/pages/tickets.js
@@ -3,49 +3,49 @@ import QRCode from "qrcode.react";
 import Image from "next/image";
 import Layout from "../components/Layout";
 import { useSelector, useDispatch } from "react-redux";
-import { getAllEventsAction, getTicketsAction } from "../services/actions/userActions";
-import { saveAs } from 'file-saver';
-import { toast } from 'react-toastify'
+import {
+  getAllEventsAction,
+  getTicketsAction,
+} from "../services/actions/userActions";
+import { saveAs } from "file-saver";
+import { toast } from "react-toastify";
 
 // Dummy NFT data (replace with real data later)
 
-
 const NftTicket = () => {
-  const { tickets, events } = useSelector(state => state.user)
-  const dispatch = useDispatch()
+  const { tickets, events } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
   const eventImage = (id) => {
     const ev = events?.filter((event) => event?._id === id)[0];
-    return ev
-  }
-
+    return ev;
+  };
 
   const downLoadTicket = (image_url) => {
-    saveAs(image_url, 'image.png')
-    toast.success('Ticket downloaded successfully')
-
-
-  }
+    saveAs(image_url, "image.png");
+    toast.success("Ticket downloaded successfully");
+  };
   useEffect(() => {
-    dispatch(getAllEventsAction())
-    dispatch(getTicketsAction())
-
-  }, [])
+    dispatch(getAllEventsAction());
+    dispatch(getTicketsAction());
+  }, []);
 
   return (
     <Layout>
       <div className="gradient fixed"></div>
       <div className="container mx-auto mt-32 px-4 tessera-header relative z-10">
-        <h1 className="text-3xl font-bold mb-4 tessera-header text-white">QRCODE Tickets</h1>
+        <h1 className="text-3xl font-bold mb-4 tessera-header text-white">
+          QRCODE Tickets
+        </h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {tickets?.map((nftTicket) => (
-
-
             <div
               key={nftTicket?._id}
               className="bg-[#111827] p-4 blog-header-image border border-gray-700"
             >
-              <h3 className="text-lg font-semibold mb-2 text-gray-300">NFT Ticket</h3>
+              <h3 className="text-lg font-semibold mb-2 text-gray-300">
+                NFT Ticket
+              </h3>
               <div className="mb-4">
                 <Image
                   src={eventImage(nftTicket?.event_id)?.image}
@@ -54,18 +54,27 @@ const NftTicket = () => {
                   height={500}
                   layout="responsive"
                   objectFit="cover"
-
                   // alt={nftTicket.eventName}
                   className="w-full h-40 object-cover rounded-md mb-2"
                 />
               </div>
               <div className="flex flex-col lg:flex-row justify-between align-baseline">
                 <div className="flex-[0.5] text-gray-400">
-                  <p className="text-[10px]">Event: {eventImage(nftTicket?.event_id)?.name}</p>
-                  <p className="text-[10px]">Location: {eventImage(nftTicket?.event_id)?.location}</p>
-                  <p className="text-[10px]">Date Paid: {nftTicket?.date.slice(0, 25)}</p>
-                  <p className="text-[10px]">Ticket Owner: {nftTicket?.customer_name}</p>
-                  <p className="text-[10px]">Payer Address: {nftTicket?.payer_address}</p>
+                  <p className="text-[10px]">
+                    Event: {eventImage(nftTicket?.event_id)?.name}
+                  </p>
+                  <p className="text-[10px]">
+                    Location: {eventImage(nftTicket?.event_id)?.location}
+                  </p>
+                  <p className="text-[10px]">
+                    Date Paid: {nftTicket?.date.slice(0, 25)}
+                  </p>
+                  <p className="text-[10px]">
+                    Ticket Owner: {nftTicket?.customer_name}
+                  </p>
+                  <p className="text-[10px]">
+                    Payer Address: {nftTicket?.payer_address}
+                  </p>
                 </div>
                 <div className="flex-[0.5] mb-4 lg:mt-0 md:mt-6 mt-5 qr-code">
                   {/* <QRCode value={nftTicket.qrCodeData} size={128} /> */}
@@ -75,7 +84,9 @@ const NftTicket = () => {
               <button
                 onClick={() => downLoadTicket(nftTicket?.qrcode_data)}
                 className="px-4 py-2 w-full rounded-sm font-semibold bg-slate-500"
-              >Download ticket</button>
+              >
+                Download ticket
+              </button>
             </div>
           ))}
         </div>
--- src/services/reducers/user.reducer.js
+++ src/services/reducers/user.reducer.js
@@ -1,75 +1,66 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { createEventTicketAction, getAllEventsAction, getTicketsAction, setCurrentEvent } from '../actions/userActions'
-
-
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  createEventTicketAction,
+  getAllEventsAction,
+  getTicketsAction,
+  setCurrentEvent,
+} from "../actions/userActions";
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState: {
-        events: null,
-        loadingState: false,
-        current_event: null,
-        purchase_ticket: null,
-        tickets: null,
+  name: "user",
+  initialState: {
+    events: null,
+    loadingState: false,
+    current_event: null,
+    purchase_ticket: null,
+    tickets: null,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getTicketsAction.pending, (state, action) => {
+      state.loadingState = true;
+    });
+    builder.addCase(getTicketsAction.fulfilled, (state, action) => {
+      state.loadingState = false;
+      state.tickets = action.payload;
+    });
+    builder.addCase(getTicketsAction.rejected, (state, action) => {
+      state.loadingState = false;
+    });
+    //
+    builder.addCase(getAllEventsAction.pending, (state, action) => {
+      state.loadingState = true;
+    });
+    builder.addCase(getAllEventsAction.fulfilled, (state, action) => {
+      state.loadingState = false;
+      state.events = action.payload;
+    });
+    builder.addCase(getAllEventsAction.rejected, (state, action) => {
+      state.loadingState = false;
+    });
+    //
+    builder.addCase(setCurrentEvent.pending, (state, action) => {
+      state.loadingState = true;
+    });
+    builder.addCase(setCurrentEvent.fulfilled, (state, action) => {
+      state.loadingState = false;
+      state.current_event = action.payload;
+    });
+    builder.addCase(setCurrentEvent.rejected, (state, action) => {
+      state.loadingState = false;
+    });
+    //
+    //
+    builder.addCase(createEventTicketAction.pending, (state, action) => {
+      state.loadingState = true;
+    });
+    builder.addCase(createEventTicketAction.fulfilled, (state, action) => {
+      state.loadingState = false;
+      state.purchase_ticket = action.payload;
+    });
+    builder.addCase(createEventTicketAction.rejected, (state, action) => {
+      state.loadingState = false;
+    });
+  },
+});
 
-
-
-    },
-    extraReducers: (builder) => {
-        builder.addCase(getTicketsAction.pending, (state, action) => {
-            state.loadingState = true
-        })
-        builder.addCase(getTicketsAction.fulfilled, (state, action) => {
-            state.loadingState = false
-            state.tickets = action.payload
-        })
-        builder.addCase(getTicketsAction.rejected, (state, action) => {
-            state.loadingState = false
-        })
-        //
-        builder.addCase(getAllEventsAction.pending, (state, action) => {
-            state.loadingState = true
-        })
-        builder.addCase(getAllEventsAction.fulfilled, (state, action) => {
-            state.loadingState = false
-            state.events = action.payload
-        })
-        builder.addCase(getAllEventsAction.rejected, (state, action) => {
-            state.loadingState = false
-        })
-        //
-        builder.addCase(setCurrentEvent.pending, (state, action) => {
-            state.loadingState = true
-        })
-        builder.addCase(setCurrentEvent.fulfilled, (state, action) => {
-            state.loadingState = false
-            state.current_event = action.payload
-        })
-        builder.addCase(setCurrentEvent.rejected, (state, action) => {
-            state.loadingState = false
-        })
-        //
-        //
-        builder.addCase(createEventTicketAction.pending, (state, action) => {
-            state.loadingState = true
-        })
-        builder.addCase(createEventTicketAction.fulfilled, (state, action) => {
-            state.loadingState = false
-            state.purchase_ticket = action.payload
-        })
-        builder.addCase(createEventTicketAction.rejected, (state, action) => {
-            state.loadingState = false
-        })
-        //
-
-        // //
-
-
-
-
-
-
-    }
-})
-
-export default userSlice.reducer
+export default userSlice.reducer;
