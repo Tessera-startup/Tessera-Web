4ec2ea86c9cd9863488c46db6d7302c39c729ef1
temycodes <temycodes@gmail.com>
2023-10-06 18:48:51 +0100

    build: code cleanup

--- src/pages/admin/events.js
+++ src/pages/admin/events.js
@@ -1,3 +1,4 @@
+import { API } from "../../services/axios_config";
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
 import Dashboard from "../../components/Dashboard";
@@ -5,22 +6,27 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { IoMdArrowBack } from "react-icons/io";
-import axios from "axios";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
 
-function AdminPage() {
+function AdminEventsPage() {
   const [events, setEvents] = useState([]);
   const [visibleEvents, setVisibleEvents] = useState(3);
+  const { authData } = useSelector((state) => state.auth);
+
+  const router = useRouter();
 
   useEffect(() => {
-    // Fetch events data when the component mounts
-    fetchEventsData();
-  }, []);
+    if (!authData || !authData.user) {
+      router.push("/login");
+    } else {
+      fetchEventsData();
+    }
+  }, [authData, router]);
 
   const fetchEventsData = async () => {
     try {
-      const response = await axios.get(
-        "https://tessera-api.onrender.com/events/user-events"
-      );
+      const response = await API.get("/events/user-events");
       setEvents(response.data);
     } catch (error) {
       console.error("Error fetching events:", error);
@@ -40,52 +46,53 @@ function AdminPage() {
         </Link>
         <div className="event-content p-0 sm:p-8 about relative z-10">
           <h2 className="text-3xl font-semibold mb-4 text-white">All Events</h2>
-          {events.slice(0, visibleEvents).map((event) => (
-            <Link href={`/event/${event.id}`} key={event.id}>
-              <motion.div
-                initial={{ scale: 0.9, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                exit={{ scale: 0.9, opacity: 0 }}
-                className="blog-card rounded shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105"
-              >
-                <div className="relative">
-                  <Image
-                    src={event?.image} // Adjust this based on your API response
-                    alt={event.title}
-                    width={640}
-                    height={480}
-                    layout="responsive"
-                    objectFit="cover"
-                    className="blog-header-image"
-                  />
-                  <div className="absolute top-0 left-0 m-2 p-1 bg-gray-800 text-white text-xs sm:text-sm rounded-md">
-                    {event.location} location
+          {Array.isArray(events) &&
+            events.slice(0, visibleEvents).map((event) => (
+              <Link href={`/event/${event.id}`} key={event.id}>
+                <motion.div
+                  initial={{ scale: 0.9, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  exit={{ scale: 0.9, opacity: 0 }}
+                  className="blog-card rounded shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105"
+                >
+                  <div className="relative">
+                    <Image
+                      src={event?.image} // Adjust this based on your API response
+                      alt={event.title}
+                      width={640}
+                      height={480}
+                      layout="responsive"
+                      objectFit="cover"
+                      className="blog-header-image"
+                    />
+                    <div className="absolute top-0 left-0 m-2 p-1 bg-gray-800 text-white text-xs sm:text-sm rounded-md">
+                      {event.location} location
+                    </div>
                   </div>
-                </div>
-                <div className="p-3">
-                  <motion.h3
-                    className="text-md sm:text-lg md:text-xl font-medium text-gray-400"
-                    whileHover={{ scale: 1.05 }}
-                  >
-                    {event.title}
-                  </motion.h3>
-                  <p className="text-gray-400">{event.date}</p>
-                  <p className="text-gray-400">{event.location}</p>
-                  <div className="flex justify-between items-center">
-                    <p className="text-sm sm:text-md md:text-lg font-semibold text-gray-400">
-                      {event.amount}
-                    </p>
-                    <motion.p
-                      className="text-sm sm:text-md md:text-lg text-gray-400 cursor-pointer"
-                      whileHover={{ textDecoration: "underline" }}
+                  <div className="p-3">
+                    <motion.h3
+                      className="text-md sm:text-lg md:text-xl font-medium text-gray-400"
+                      whileHover={{ scale: 1.05 }}
                     >
-                      Get Ticket
-                    </motion.p>
+                      {event.title}
+                    </motion.h3>
+                    <p className="text-gray-400">{event.date}</p>
+                    <p className="text-gray-400">{event.location}</p>
+                    <div className="flex justify-between items-center">
+                      <p className="text-sm sm:text-md md:text-lg font-semibold text-gray-400">
+                        {event.amount}
+                      </p>
+                      <motion.p
+                        className="text-sm sm:text-md md:text-lg text-gray-400 cursor-pointer"
+                        whileHover={{ textDecoration: "underline" }}
+                      >
+                        Get Ticket
+                      </motion.p>
+                    </div>
                   </div>
-                </div>
-              </motion.div>
-            </Link>
-          ))}
+                </motion.div>
+              </Link>
+            ))}
         </div>
         {visibleEvents < events.length && (
           <div className="flex justify-center mb-10 text-gray-100">
@@ -113,4 +120,4 @@ function AdminPage() {
   );
 }
 
-export default AdminPage;
+export default AdminEventsPage;
